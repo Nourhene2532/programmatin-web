254d279c80703bc48164b9dc68ddedca7fdb091b
Nourhene2532 <nourhenesaidani702@gmail.com>
2024-12-21 11:37:21 -0100

    Add files via upload

---
+++ controle.js
@@ -0,0 +1,9 @@
+const prenom = document.getElementById("prenom");
+const nom = document.getElementById("nom");
+const email = document.getElementById("email");
+const mot de passe= document.getElementById("mot de passe");
+const genre = document.getElementById("genre");
+const pays= document.getElementById("pays");
+let btn = document.getElementsByCId("btn");
+btn.addEvenlistener("Click",verif);
+function
